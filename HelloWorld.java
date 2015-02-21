f6cad29b310b3f42eadf63dbca73d33012711a67
Olaf Dietsche <olafdietsche@users.noreply.github.com>
2015-02-21 18:53:34 +0100

    Use auto_increment for id column

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,6 +7,8 @@ import javax.persistence.Id;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.*;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 import javax.persistence.Table;
@@ -15,6 +17,7 @@ import javax.persistence.Table;
 @Table(name = "contacts")
 class Contact {
 	@Id
+	@GeneratedValue(strategy = SEQUENCE)
 	public int id;
 	public String name;
 	public String email;
@@ -22,9 +25,8 @@ class Contact {
 }
 
 public class HelloWorld {
-	public static Contact createContact(int id, String name, String email, String phone) {
+	public static Contact createContact(String name, String email, String phone) {
 		Contact contact = new Contact();
-		contact.id = id;
 		contact.name = name;
 		contact.email = email;
 		contact.phone = phone;
@@ -38,17 +40,16 @@ public class HelloWorld {
 
 		tx.begin();
 
-		Contact contact = createContact(7, "John Doe", "john@example.com", "555-123-4567");
+		Contact contact = createContact("John Doe", "john@example.com", "555-123-4567");
 		em.persist(contact);
 		em.flush();
 		tx.commit();
 
 		tx.begin();
-		Query insert = em.createNativeQuery("insert into contacts(id, name, email, phone) values(?, ?, ?, ?)");
-		insert.setParameter(1, 8);
-		insert.setParameter(2, "Jane Doe");
-		insert.setParameter(3, "jane@example.com");
-		insert.setParameter(4, "555-456-7890");
+		Query insert = em.createNativeQuery("insert into contacts(name, email, phone) values(?, ?, ?)");
+		insert.setParameter(1, "Jane Doe");
+		insert.setParameter(2, "jane@example.com");
+		insert.setParameter(3, "555-456-7890");
 		insert.executeUpdate();
 		tx.rollback();
 
